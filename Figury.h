19c59d45c1fc0348dbc17488f1bf4252e5d8d35c
tomk132 <kucinskitomasz@gmail.com>
2026-01-06 10:56:38 +0100

    Zadanie 7 - OK

--- Figury.h
+++ Figury.h
@@ -6,25 +6,24 @@ class Figura
 {
 
 public:
-    //void set(double pole_nowy) { pole = pole_nowy; }
+    // void set(double pole_nowy) { pole = pole_nowy; }
 
+    Figura(double pole_in) : pole{pole_in} {};
     double get() const { return pole; }
 
-//private:
-    protected: 
+private:
+    // protected:
     double pole;
 };
 
-
 class Kolo : public Figura
 {
-    public: 
-    Kolo(double r) { pole = std::numbers::pi * r * r; };
+public:
+    Kolo(double r) : Figura(std::numbers::pi * r * r) {};
 };
-
 
 class Kwadrat : public Figura
 {
-    public: 
-    Kwadrat(double bok) { pole = bok * bok; };
+public:
+    Kwadrat(double bok) : Figura(bok * bok) {};
 };
