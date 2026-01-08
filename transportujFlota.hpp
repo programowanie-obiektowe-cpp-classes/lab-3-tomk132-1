2b4e421f707ff20c59a519bc97aa84ee30c7be6f
tomk132 <kucinskitomasz@gmail.com>
2026-01-08 12:16:01 +0100

    Zejściówka - OK

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,35 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+
+    unsigned int TransportSuma{0}; 
+
+    //utworzenie fabryki
+    Stocznia s; 
+
+    unsigned int licznik_zaglowiec{0}; 
+
+
+    while (TransportSuma < towar && towar != 0)
+    {
+    
+    Statek* StatekLive = s();      
+
+    if (dynamic_cast< Zaglowiec* > (StatekLive) != nullptr) {
+        licznik_zaglowiec++;
+    }; 
+
+    TransportSuma = TransportSuma + StatekLive->transportuj(); 
+
+
+
+    delete StatekLive; 
+
+
+
+    }; 
+
+
+
+    return licznik_zaglowiec;
 }
